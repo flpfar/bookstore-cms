f8fd5c8005b6e2f03f09afff2622f6bc7903781f
Zubenna <emelunna@gmail.com>
2021-03-31 01:02:14 +0100

    Defining Redux Actions And Reducers

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,15 +2,16 @@
 const CREATE_BOOK = 'CREATE_BOOK';
 const REMOVE_BOOK = 'REMOVE_BOOK';
 
-const createBook = book => {
-    return {
-      type: CREATE_BOOK,
-      payload: book,
-    }
-  }
-  const removeBook = book => {
-    return {
-      type: REMOVE_BOOK,
-      payload: book,
-    }
-  }
+const createBook = book => ({
+  type: CREATE_BOOK,
+  book,
+});
+
+const removeBook = book => ({
+  type: REMOVE_BOOK,
+  book,
+});
+
+export {
+  CREATE_BOOK, REMOVE_BOOK, createBook, removeBook,
+};
--- src/reducers/books.js
+++ src/reducers/books.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import { CREATE_BOOK, REMOVE_BOOK } from '../actions/index';
 
-const books = (book) => {
-    return (
-        <div>
-           {}
-        </div>
-    );
+const bookReducer = (state = [], action) => {
+  switch (action.type) {
+    case CREATE_BOOK:
+      return [...state, action.id];
+    case REMOVE_BOOK:
+      return state.filter(book => book.id !== parseInt(action.id, 2));
+    default:
+      return state;
+  }
 };
 
-export default books;
+export default bookReducer;
